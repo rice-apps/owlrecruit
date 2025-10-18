374d748c78dd499d50cc3785ddb1e2414c3a4062
chu-andrew <chu.andrew.h@gmail.com>
2025-10-17 22:32:48 -0500

    rm tutorial and vercel button

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
-import { DeployButton } from "@/components/deploy-button";
 import { AuthButton } from "@/components/auth-button";
 import { Hero } from "@/components/hero";
 import { ThemeSwitcher } from "@/components/theme-switcher";
-import { SignUpUserSteps } from "@/components/tutorial/sign-up-user-steps";
 import Link from "next/link";
 
 export default function Home() {
@@ -13,19 +11,12 @@ export default function Home() {
           <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
             <div className="flex gap-5 items-center font-semibold">
               <Link href={"/"}>Next.js Supabase Starter</Link>
-              <div className="flex items-center gap-2">
-                <DeployButton />
-              </div>
             </div>
             <AuthButton />
           </div>
         </nav>
         <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5">
           <Hero />
-          <main className="flex-1 flex flex-col gap-6 px-4">
-            <h2 className="font-medium text-xl mb-4">Next steps</h2>
-            <SignUpUserSteps />
-          </main>
         </div>
 
         <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
