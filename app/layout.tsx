5df4a994270fa4f022894bc8a0f6ca01087337f4
chu-andrew <chu.andrew.h@gmail.com>
2025-10-17 22:49:14 -0500

    init owlrecruit

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,14 +3,12 @@ import { Geist } from "next/font/google";
 import { ThemeProvider } from "next-themes";
 import "./globals.css";
 
-const defaultUrl = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
-  : "http://localhost:3000";
+const defaultUrl = "http://localhost:3000";
 
 export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "OwlRecruit",
+  description: "A centralized recruitment platform for organizations at Rice University.",
 };
 
 const geistSans = Geist({
--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
         <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
           <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
             <div className="flex gap-5 items-center font-semibold">
-              <Link href={"/"}>Next.js Supabase Starter</Link>
+              <Link href={"/"}>OwlRecruit</Link>
             </div>
             <AuthButton />
           </div>
@@ -21,14 +21,14 @@ export default function Home() {
 
         <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
           <p>
-            Powered by{" "}
+            Built by {" "}
             <a
-              href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
+              href="https://riceapps.org/"
               target="_blank"
               className="font-bold hover:underline"
               rel="noreferrer"
             >
-              Supabase
+              RiceApps
             </a>
           </p>
           <ThemeSwitcher />
