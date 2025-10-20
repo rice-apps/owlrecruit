14232ab6e799500b799017496dff30d066054948
Leif MacCarthy <leif@lmgroup.dev>
2025-10-20 13:50:57 -0500

    finished google sso auth, feature working on local instance

--- app/auth/sign-up/sign-up-google.tsx
+++ app/auth/sign-up/sign-up-google.tsx
@@ -1,25 +1,54 @@
-'use client';
-import { useState } from 'react';
-import { useEffect } from 'react';
-import { createClient } from '@/lib/supabase/client';
+"use client";
+import { useCallback, useEffect, useState } from "react";
+import { createClient } from "@supabase/supabase-js";
+import Script from 'next/script'
 
-export default function GoogleAuthSetup() {
+const supabase = createClient(
+    // TODO: add error handling if keys are missing
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!
+);
+declare global {
+  interface Window {
+    handleSignInWithGoogle: (response: { credential: string }) => Promise<void>;
+  }
+}
+
+export default function SignUpGoogleBtn() {
+    const handleSignInWithGoogle = useCallback(async (response: { credential: string }) => {
+    try {
+        const { data, error } = await supabase.auth.signInWithIdToken({
+            // problem with data handling after sign in,
+            // test w/ actual supabase instance or reinstall supabase image to account for new changes
+            provider: 'google',
+            token: response.credential,
+            });
+            if (error) console.error(error.message);
+        } catch (err) { console.error(err)}}, [])
     
     useEffect(() => {
-    //     declare global {
-    //         interface Window {
-    //             handleSignInWithGoogle: (response: { credential: any }) => Promise<void>;
-    //   }
-    // }
-    (window as any).handleSignInWithGoogle = async function (response: { credential: any }) {
-        const supabase = await createClient();
-        const { data, error } = await supabase.auth.signInWithIdToken({
-        provider: 'google',
-        token: response.credential,
-    })
-};
-}, []);
+        window.handleSignInWithGoogle = handleSignInWithGoogle;
+    }, [handleSignInWithGoogle]);
 
-    return null;
+    return (
+        <>
+            <Script src="https://accounts.google.com/gsi/client" strategy="afterInteractive" />
+            <div id="g_id_onload"
+              data-client_id="411999379081-n4h1dh8au1a7a861a13o00irbje4mjp6.apps.googleusercontent.com"
+              data-context="signup"
+              data-ux_mode="popup"
+              data-callback="handleSignInWithGoogle"
+              data-auto_prompt="false">
+            </div>
+
+            <div className="g_id_signin"
+                data-type="standard"
+                data-shape="pill"
+                data-theme="filled_blue"
+                data-text="signin_with"
+                data-size="large"
+                data-logo_alignment="left">
+            </div>
+          </>
+    )
 }
-    
--- components/sign-up-form.tsx
+++ components/sign-up-form.tsx
@@ -1,8 +1,6 @@
 "use client";
 
 import { cn } from "@/lib/utils";
-import { createClient } from "@/lib/supabase/client";
-import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
@@ -10,68 +8,23 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import Link from "next/link";
-import { useRouter } from "next/navigation";
-import { useState, useEffect } from "react";
-import Script from "next/script";
-import GoogleAuthSetup from "@/app/auth/sign-up/sign-up-google";
+import SignUpGoogleBtn from "@/app/auth/sign-up/sign-up-google";
 
 
 export function SignUpForm({
   className,
   ...props
 }: React.ComponentPropsWithoutRef<"div">) {
-  const [loaded, setLoaded] = useState(false);
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [repeatPassword, setRepeatPassword] = useState("");
-  const [error, setError] = useState<string | null>(null);
-  const [isLoading, setIsLoading] = useState(false);
-  const router = useRouter();
-  // useEffect(() => {
-  //       const script = document.createElement('script');
-  //       script.src = 'https://accounts.google.com/gsi/client';
-  //       script.async = true;
-  //       script.defer = true;
-  //       script.onload = () => setLoaded(true);
-  // })
-  // if (!loaded) return null;
+
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
-      <GoogleAuthSetup />
-      {/* <Script id="handleSignInWithGoogle">{`
-          
-    const supabase = await createClient();
-    const { data, error } = await supabase.auth.signInWithIdToken({
-    provider: 'google',
-    token: response.credential,
-  })
-      `}</Script> */}
-      <script src="https://accounts.google.com/gsi/client" async />
       <Card>
         <CardHeader>
           <CardTitle className="text-2xl">Sign up</CardTitle>
           <CardDescription></CardDescription>
         </CardHeader>
         <CardContent>
-          <div id="g_id_onload"
-              data-client_id="411999379081-n4h1dh8au1a7a861a13o00irbje4mjp6.apps.googleusercontent.com"
-              data-context="signup"
-              data-ux_mode="popup"
-              data-callback="handleSignInWithGoogle"
-              data-auto_prompt="false">
-          </div>
-
-          <div className="g_id_signin"
-              data-type="standard"
-              data-shape="pill"
-              data-theme="filled_blue"
-              data-text="signin_with"
-              data-size="large"
-              data-logo_alignment="left">
-          </div>
+          <SignUpGoogleBtn />
         </CardContent>
       </Card>
     </div>
