18febae9c24e9a4c14fd7abc0268db483b24ecaf
Angel-Ern-Lopez <lopezangel05@icloud.com>
2025-10-25 15:50:59 -0500

    Outlined the landing page, which needs styled.

--- app/page.tsx
+++ app/page.tsx
@@ -15,9 +15,40 @@ export default function Home() {
             <AuthButton />
           </div>
         </nav>
+        {/* Hero Section of the landing page. Include a call to action */}
         <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5">
           <Hero />
+          
         </div>
+
+        {/* Description Section */}
+        <div className="flex-1 flex flex-col gap-4 max-w-5xl p-5">
+          <p className="text-3xl lg:text-3xl !leading-tight mx-auto max-w-xl text-center">
+            The smarter way to connect students with clubs:
+          </p>
+          <p className="text-3xl lg:text-2xl !leading-tight mx-auto max-w-xl text-center">
+            OwlRecuit brings club recruiting into the modern age. Browse open applications, 
+            track your submissions, and manage reviews—all in one place! No more lost emails, 
+            missed deadlines, or endless spreadsheets. Whether you're finding your community 
+            or building it, OwlRecruit makes it effortless.
+          </p>
+        </div>
+
+        {/* Contact Section*/}
+        <div className="flex-1 flex flex-col gap-4 max-w-5xl p-5">
+          <p className="text-3xl lg:text-4xl !leading-tight mx-auto max-w-xl text-center">
+            Contacts:
+          </p>
+        </div>
+        
+       {/* FAQ Section*/}
+        <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5">
+          <p className="text-3xl lg:text-4xl !leading-tight mx-auto max-w-xl text-center">
+            FAQ
+          </p>
+        </div>
+
+
 
         <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
           <p>
--- components/hero.tsx
+++ components/hero.tsx
@@ -1,10 +1,20 @@
+import { Button } from "./ui/button";
+
 export function Hero() {
   return (
     <div className="flex flex-col gap-16 items-center">
       <h1 className="sr-only">Supabase and Next.js Starter Template</h1>
       <p className="text-3xl lg:text-4xl !leading-tight mx-auto max-w-xl text-center">
-        hi world
+        Welcome to OwlRecruit!
       </p>
+      <p className="text-3xl lg:text-4xl !leading-tight mx-auto max-w-xl text-center">
+        Rice University's club application portal. 
+      </p>
+      <Button>
+        Browse Campus Opportunities
+      </Button>
+
+
     </div>
   );
 }
