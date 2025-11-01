0691edd2f10394fab165ccff9f42d37fb0b95c8b
Leif MacCarthy <leif@lmgroup.dev>
2025-11-01 14:54:49 -0500

    admin page has file upload, no drag drop functionality, middleware implemented

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,40 +1,27 @@
-import { AuthButton } from "@/components/auth-button";
-import { Hero } from "@/components/hero";
-import { ThemeSwitcher } from "@/components/theme-switcher";
-import Link from "next/link";
+'use client'
+
+import {uploadCSV} from '../api/upload/upload-csv';
+import readCSVAsString from '../api/upload/upload-csv';
 
 export default function ApplicationUpload() {
+  const handleFileChange = async (e: any) => {
+    const file = e.target.files[0];
+    if (file) {
+      try {
+      const csvStr = await readCSVAsString(file);
+      uploadCSV(csvStr);
+      } catch (error) {
+        console.error("Couldn't read file: ", error);
+      }
+    }
+  };
   return (
-    <main className="min-h-screen flex flex-col items-center">
-      <div className="flex-1 w-full flex flex-col gap-20 items-center">
-        <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-          <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
-            <div className="flex gap-5 items-center font-semibold">
-              <Link href={"/"}>OwlRecruit</Link>
-            </div>
-            <AuthButton />
-          </div>
-        </nav>
-
-        <p className="text-3xl lg:text-2xl !leading-tight mx-auto max-w-xl text-center">
-          Application Upload:
-        </p>
-       
-        <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
-          <p>
-            Built by {" "}
-            <a
-              href="https://riceapps.org/"
-              target="_blank"
-              className="font-bold hover:underline"
-              rel="noreferrer"
-            >
-              RiceApps
-            </a>
-          </p>
-          <ThemeSwitcher />
-        </footer>
-      </div>
-    </main>
+    <>
+    <input 
+      type="file" 
+      onChange={handleFileChange}
+      accept=".csv,.txt"
+    />
+    </>
   );
 }
---
+++ app/api/upload/upload-csv.tsx
@@ -0,0 +1,30 @@
+export const uploadCSV = async (csvStr: string) => {
+    const reader = new FileReader();
+    const response = await fetch('/api/upload/applications', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'text/plain',
+        },
+        body: csvStr
+//         body: `org_id,applicant_id,status,notes,created_at,updated_at,position,form_responses
+// 006223f5-589e-4886-a3c9-11b90fa99f3c,7aed92ea-4fbb-4d65-b9f1-001a567adcb5,Applied,Lacking SQL experience,2025-10-18 20:42:58.720151+00,2025-10-18 20:42:58.720151+00,New Dev,`
+    })
+
+    const result = await response.json();
+}
+export default function readCSVAsString(file: File): Promise<string> {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    
+    reader.onload = (event) => {
+      const csvString = event.target?.result as string;
+      resolve(csvString);
+    };
+    
+    reader.onerror = (error) => {
+      reject(error);
+    };
+    
+    reader.readAsText(file);
+  });
+}
