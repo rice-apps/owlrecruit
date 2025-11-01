e0d39ba93f9a229beac4c7f76a50556548dbfaa8
Angel-Ern-Lopez <lopezangel05@icloud.com>
2025-11-01 13:49:33 -0500

    Outline for Application Upload page

---
+++ app/admin/page.tsx
@@ -0,0 +1,40 @@
+import { AuthButton } from "@/components/auth-button";
+import { Hero } from "@/components/hero";
+import { ThemeSwitcher } from "@/components/theme-switcher";
+import Link from "next/link";
+
+export default function ApplicationUpload() {
+  return (
+    <main className="min-h-screen flex flex-col items-center">
+      <div className="flex-1 w-full flex flex-col gap-20 items-center">
+        <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
+          <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
+            <div className="flex gap-5 items-center font-semibold">
+              <Link href={"/"}>OwlRecruit</Link>
+            </div>
+            <AuthButton />
+          </div>
+        </nav>
+
+        <p className="text-3xl lg:text-2xl !leading-tight mx-auto max-w-xl text-center">
+          Application Upload:
+        </p>
+       
+        <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
+          <p>
+            Built by {" "}
+            <a
+              href="https://riceapps.org/"
+              target="_blank"
+              className="font-bold hover:underline"
+              rel="noreferrer"
+            >
+              RiceApps
+            </a>
+          </p>
+          <ThemeSwitcher />
+        </footer>
+      </div>
+    </main>
+  );
+}
