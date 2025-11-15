caece3ab2676a2678418846c1d0fbc81e244cc4c
Leif MacCarthy <leif@lmgroup.dev>
2025-11-15 14:10:25 -0600

    Basic file structure

---
+++ app/protected/dashboard/clubs/positions/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+export default function OpenPositions() {
+return (
+    <>
+    <div className="flex-1 w-full flex flex-col gap-12">
+      {/* Open Positions Header */}
+      <div>
+        <h1 className="font-bold text-3xl mb-4">Open Positions</h1>
+        </div>
+        </div>
+    </>
+);
+}
