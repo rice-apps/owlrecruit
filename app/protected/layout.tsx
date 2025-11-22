d149ea14629a368906cbe5a87248215515bcfb41
GraceYang9127 <138538325+GraceYang9127@users.noreply.github.com>
2025-11-22 13:34:25 -0600

    routing when logged in

--- app/protected/layout.tsx
+++ app/protected/layout.tsx
@@ -12,7 +12,7 @@ export default function ProtectedLayout({
         <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
           <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
             <div className="flex gap-5 items-center font-semibold">
-              <Link href={"/"}>OwlRecruit</Link>
+              <Link href={"/protected"}>OwlRecruit</Link>
             </div>
           </div>
         </nav>
