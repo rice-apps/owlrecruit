ffce03b08cc114c1ee4d87e5512346c76f9ac34d
chu-andrew <chu.andrew.h@gmail.com>
2025-12-16 22:29:23 -0600

    clean uploadCSV

--- app/api/upload/upload-csv.tsx
+++ app/api/upload/upload-csv.tsx
@@ -1,11 +1,9 @@
 export const uploadCSV = async (csv: File, path: string) => {
-    const reader = new FileReader();
-    const response = await fetch(path, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'text/csv',
-        },
-        body: csv
-      })
-    const result = await response.json();
+  await fetch(path, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'text/csv',
+    },
+    body: csv
+  })
 }
