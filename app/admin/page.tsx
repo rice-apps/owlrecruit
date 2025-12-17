cb155600dc7d302b81216811c979c4d7898942d7
chu-andrew <chu.andrew.h@gmail.com>
2025-12-16 22:20:32 -0600

    ApplicationUpload

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,14 +1,14 @@
 'use client'
 
 import {uploadCSV} from '../api/upload/upload-csv';
-import { Button } from "../../components/ui/button";
+import {Button} from "@/components/ui/button";
 
 export default function ApplicationUpload() {
   const handleFileChangeInterview = async (e: any) => {
     const file = e.target.files[0];
     if (file) {
       try {
-        uploadCSV(file, "/api/interviews");
+        await uploadCSV(file, "/api/interviews");
       } catch (error) {
         console.error("Couldn't read file: ", error);
       }
@@ -18,38 +18,40 @@ export default function ApplicationUpload() {
     const file = e.target.files[0];
     if (file) {
       try {
-      uploadCSV(file, "/api/interviews");
+        await uploadCSV(file, "/api/interviews");
       } catch (error) {
         console.error("Couldn't read file: ", error);
       }
     }
   };
-  
+
   return (
     <>
-    <h2 className="font-bold text-2xl mb-4">Hey admin!</h2>
-    <Button asChild size="lg">
-      <label htmlFor="interviews-upload" style={{ cursor: 'pointer'}}>
-        Upload Interview Feedback
-      </label>
-    </Button>
-    <input 
-      type="file"
-      id="interviews-upload"
-      onChange={handleFileChangeInterview}
-      accept=".csv,.txt"
-      style={{ display: 'none' }}
-    />
-    <Button asChild size="lg">
-      <label htmlFor="applications-upload" style={{ cursor: 'pointer'}}>
-        Upload Applications
-      </label>
-    </Button>
-    <input 
-      type="file"
-      id="applications-upload"
-      onChange={handleFileChangeApplications}
-      accept=".csv,.txt"
-      style={{ display: 'none' }}
+      <h2 className="font-bold text-2xl mb-4">Hey admin!</h2>
+      <Button asChild size="lg">
+        <label htmlFor="interviews-upload" style={{cursor: 'pointer'}}>
+          Upload Interview Feedback
+        </label>
+      </Button>
+      <input
+        type="file"
+        id="interviews-upload"
+        onChange={handleFileChangeInterview}
+        accept=".csv,.txt"
+        style={{display: 'none'}}
+      />
+      <Button asChild size="lg">
+        <label htmlFor="applications-upload" style={{cursor: 'pointer'}}>
+          Upload Applications
+        </label>
+      </Button>
+      <input
+        type="file"
+        id="applications-upload"
+        onChange={handleFileChangeApplications}
+        accept=".csv,.txt"
+        style={{display: 'none'}}
+      />
+    </>
   );
 }
