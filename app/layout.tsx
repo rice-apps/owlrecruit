6d2a4754c61ab1c47ed0c11ec0949f7cb78e0d3b
Michael McNeil <mg188@rice.edu>
2026-02-07 15:22:38 -0600

    chore: move global footer to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,31 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          {children}
+          <div className="min-h-screen flex flex-col">
+            <div className="flex-1">{children}</div>
+
+            <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs py-16">
+              <p className="text-muted-foreground">
+                Made with{" "}
+                <span role="img" aria-label="love">
+                  ❤️
+                </span>{" "}
+                by{" "}
+                <a
+                  href="https://riceapps.org/"
+                  target="_blank"
+                  className="font-bold hover:underline text-foreground"
+                  rel="noreferrer"
+                >
+                  RiceApps
+                </a>
+                .{" "}
+                <span className="text-muted-foreground">
+                  Last updated on February 7, 2026.
+                </span>
+              </p>
+            </footer>
+          </div>
         </ThemeProvider>
       </body>
     </html>
