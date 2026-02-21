dc5337c5f6be2f9c4e53bec3169c19767d5853e3
Leif MacCarthy <leif@lmgroup.dev>
2026-02-21 13:43:50 -0600

    merge issues

--- app/protected/applications/page.tsx
+++ app/protected/applications/page.tsx
@@ -98,9 +98,6 @@ export default function MyApplicationsPage() {
         />
         <Search className="absolute right-4 top-1/2 transform -translate-y-1/2 text-gray-400 w-5 h-5" />
       </div>
-    );
-  }
-
       {/* Loading State */}
       {loading && (
         <div className="flex items-center justify-center py-12">
