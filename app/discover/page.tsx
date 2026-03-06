d5aaeb9bf3f61e0f0ba7902b6e608f42d9409012
chu-andrew <chu.andrew.h@gmail.com>
2026-03-06 16:35:59 -0600

    sidebar

--- app/discover/page.tsx
+++ app/discover/page.tsx
@@ -14,7 +14,7 @@ export default function PublicDiscoverPage() {
                 viewBox="0 0 24 24"
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
-                className="w-full h-full text-cyan-500"
+                className="w-full h-full text-owl-purple"
               >
                 <path
                   d="M12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2ZM12 20C7.59 20 4 16.41 4 12C4 7.59 7.59 4 12 4C16.41 4 20 7.59 20 12C20 16.41 16.41 20 12 20Z"
@@ -29,7 +29,7 @@ export default function PublicDiscoverPage() {
                 />
               </svg>
             </div>
-            <span className="font-bold text-xl text-cyan-500 tracking-tight">
+            <span className="font-bold text-xl text-owl-purple tracking-tight">
               owlrecruit
             </span>
           </Link>
